b222c94e0bc9103bbb9a8995be5064292f45dbc7
Rishabh Suny <rishabhsunyriz@gmail.com>
2024-09-12 14:26:46 +0000

    .gitignore

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#*j@3nw_sz@^v71r_(@_jbnfgr*l0=vrn4nj9#$c$&uh@*s753'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-rsuny-shortscapstone-m7ppzw9g14v.ws.codeinstitute-ide.net', 
 '.herokuapp.com']
