35515886883cef0f9df628a6985993d1c027b4ae
Rishabh Suny <rishabhsunyriz@gmail.com>
2024-09-12 16:27:43 +0000

    deployment settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -89,8 +89,8 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("postgres://ucag04zla1v:xmRQJ5LfJJES@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/aloha_zebra_glue_229738"))
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
